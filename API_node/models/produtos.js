d2b4add2c7ae0f8b155683c8055d97a4e0b229a3
Bela <imcdsls10@gmail.com>
2025-11-13 12:49:28 -0300

    Conexão tabela produtos

---
+++ API_node/models/produtos.js
@@ -0,0 +1,31 @@
+const db = require('./db');
+const produtos = db.sequelize.define('produtos', {
+    id_produtos: {
+        type: db.Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome_produtos: {
+        type: db.Sequelize.TEXT,
+        allowNull: false
+    },
+    fk_tipo_p: {
+        type:  db.Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'tipo_produto', key: 'id_tipo'},
+        onDelete: 'CASCADE',
+        allowNull: false
+    },
+    unidade_medida: {
+        type: db.Sequelize.TEXT,
+        allowNull: false,
+    },
+    preco_produto: {
+        type: db.Sequelize.DECIMAL,
+        allowNull: false
+    }
+}, {freezeTableName: true});
+
+produtos.sync({force: false});
+module.exports = produtos;
